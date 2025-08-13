3336065a4968f20a3486760c0ffa35e710407044
Fajar <siunyil302@gmail.com>
2025-08-13 22:58:33 +0700

    First commit

--- test/OS_Devices.js
+++ test/OS_Devices.js
@@ -3,7 +3,7 @@ import { expect } from "chai";
 describe('Testing Devices API Demos', () => {
     it('Mencoba Test OS Devices by API Demos', async () => {
         console.log('Testing OS Devices');
-        expect(true).to.equal(true);
+        expect(true).to.be.true
         // locator
         const buttonOS = await $('//android.widget.TextView[@content-desc="OS"]');
         await buttonOS.click();
